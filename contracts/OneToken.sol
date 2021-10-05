d3e03b440028caef11117edb09cdd0d9755ce300
Elena Kalichkina <elena.kalichkina.rho@gmail.com>
2021-10-05 18:36:39 +0700

    init

---
+++ contracts/OneToken.sol
@@ -0,0 +1,68 @@
+pragma solidity ^0.8.4;
+
+contract OneToken {
+    mapping(address => uint256) balances;
+    mapping(address => mapping(address => uint256)) allowed;
+    uint256 totalSupply_;
+
+    modifier enoughTokensOnBalance(uint256 numberTokens, address owner) {
+        if (balances[owner] >= numberTokens) _;
+    }
+
+    modifier enoughAllowedTokens(
+        address owner,
+        address spender,
+        uint256 numberTokens
+    ) {
+        if (numberTokens < allowance(owner, spender)) _;
+    }
+
+    constructor(uint256 total) {
+        totalSupply_ = total;
+        balances[msg.sender] = total;
+    }
+
+    function totalSupply() public view returns (uint256) {
+        return totalSupply_;
+    }
+
+    function getBalance(address owner) public view returns (uint256) {
+        return balances[owner];
+    }
+
+    function transferFrom(
+        address from,
+        address to,
+        uint256 numberTokens
+    )
+        public
+        enoughTokensOnBalance(numberTokens, from)
+        enoughAllowedTokens(from, to, numberTokens)
+        returns (bool)
+    {
+        balances[from] -= numberTokens;
+        allowed[from][to] -= numberTokens;
+        balances[to] += numberTokens;
+        return true;
+    }
+
+    function transfer(address to, uint256 numberTokens) public returns (bool) {
+        return transferFrom(msg.sender, to, numberTokens);
+    }
+
+    function approve(address spender, uint256 numberTokens)
+        public
+        returns (bool)
+    {
+        allowed[msg.sender][spender] += numberTokens;
+        return true;
+    }
+
+    function allowance(address owner, address spender)
+        public
+        view
+        returns (uint256)
+    {
+        return allowed[owner][spender];
+    }
+}
